293cb6a678832c8283c9290fe474df094cd8260e
shilpipawar <shilpipawar@gmail.com>
2018-12-19 00:46:48 -0600

    routing

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 // Dependencies
+var bodyParser = require("body-parser");
 var express = require("express");
+var path = require("path");
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 3000;
-
 // Create express app instance.
 var app = express();
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./app/routing/htmlRoutes.js')(app);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
